4bf1d0fd872a82b7b8261042db5b78d35e195a74
dim <web.best.master@gmail.com>
2024-05-18 18:04:40 +0200

    update for typescript project

--- dist/export.d.ts
+++
@@ -1,2 +0,0 @@
-export { Petsdb } from "./src/database";
-export type { PetsdbInitialConfigType, PetsdbItemType, PetsdbQueryType, PetsdbReadPageConfigType, PetsdbReadPageResultType, PetsdbSortDirectionType, PetsdbSortType, PetsdbSortValueType, } from "./src/database-type";
--- dist/src/queue.d.ts
+++
@@ -1,9 +0,0 @@
-export type PromiseResolveType<Result> = (result: Result) => unknown;
-export type QueueRunningTaskType = () => Promise<unknown>;
-export declare class Queue {
-    private readonly taskList;
-    private isWorking;
-    constructor();
-    add(runningTask: QueueRunningTaskType): Promise<undefined>;
-    private run;
-}
