f87137b5e0ee45627409f0acac59ff4efe45779c
dim <web.best.master@gmail.com>
2024-05-18 18:22:15 +0200

    update for typescript project

--- dist/export.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Petsdb = void 0;
-var database_1 = require("./src/database");
-Object.defineProperty(exports, "Petsdb", { enumerable: true, get: function () { return database_1.Petsdb; } });
-//# sourceMappingURL=export.js.map
